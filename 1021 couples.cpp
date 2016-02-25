2e9fcd318d548d219367b0c05a2da72d02678d52
zwbzzp <569766197@qq.com>
2016-02-25 22:12:39 +0800

    利用栈，类似括号匹配

--- 1021 couples.cpp
+++ 1021 couples.cpp
@@ -9,71 +9,38 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 int main(){
-    vector<int> s;
+    int couples[200002];
     int N;
     cin>>N;
     while (N>0) {
-        for (int i=0; i<2*N; i++) {
-            s.push_back(0);
-        }
         int a, b;
+        stack<int> s;
         for (int i=0; i<N; i++) {
             cin>>a>>b;
-            s[a-1] = i;
-            s[b-1] = i;
+            couples[a-1] = i;
+            couples[b-1] = i;
         }
-        vector<int>::iterator it;
-        vector<int>::iterator is;
-        //    for (it=s.begin(); it!=s.end(); it++) {
-        //        cout<<*it<<" ";
-        //    }
-        //    cout<<endl;
-        int num;
-        it = s.begin();
-        while (!s.empty() && it!=s.end()) {
-            num = s.size();
-            if (it==s.end()-1) {
-                if (*s.begin()==*(s.end()-1)) {
-                    s.pop_back();
-                    s.erase(s.begin());
-                    it = s.begin();
-                }
-                else if (num==s.size())
-                    break;
-                else
-                    it = s.begin();
+        for (int i=0; i<2*N; i++) {
+            if (s.empty()) {
+                s.push(couples[i]);
             }
-            
-            if (*it==*(it+1)) {
-                s.erase(it);
-                s.erase(it);
-            }
-            else
-                it++;
-            
-            if (it==s.end()) {
-                if (num==s.size()) {
-                    break;
+            else{
+                if (s.top()==couples[i]) {
+                    s.pop();
                 }
                 else
-                    it = s.begin();
+                    s.push(couples[i]);
             }
-            
-            
         }
-        //    for (it=s.begin(); it!=s.end(); it++) {
-        //        cout<<*it<<" ";
-        //    }
-        //    cout<<endl;
         if (s.empty()) {
             cout<<"Yes"<<endl;
         }
         else
             cout<<"No"<<endl;
-        s.clear();
         cin>>N;
     }
     return 0;
